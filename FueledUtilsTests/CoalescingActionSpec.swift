a2ef374deb2f8299b3aeab04c55416130f1b8af5
Stéphane Copin <stephane@fueled.com>
2019-12-09 13:22:02 -0500

    WIP

--- FueledUtilsTests/CoalescingActionSpec.swift
+++ FueledUtilsTests/CoalescingActionSpec.swift
@@ -15,6 +15,50 @@ import XCTest
 class CoalescingActionSpec: QuickSpec {
 	override func spec() {
 		describe("CoalescingAction") {
+			describe("apply().start()") {
+				it("should start the action once for each apply() until completed") {
+					var counter = 0
+					let producer = SignalProducer<Void, Never> { observer, lifetime in
+						counter += 1
+						lifetime += SignalProducer(value: ()).delay(0.1, on: QueueScheduler.main).startWithValues { _ in
+							observer.send(value: ())
+							observer.sendCompleted()
+						}
+					}
+					let action = CoalescingAction { producer }
+					expect(counter) == 0
+					action.apply().start()
+					expect(counter) == 1
+					action.apply().start()
+					expect(counter) == 1
+					action.apply().start()
+				}
+				it("should support chaining calls") {
+					let action = InputCoalescingAction<Int, Int, Never> { input in
+						SignalProducer<Int, Never> { observer, lifetime in
+							lifetime += SignalProducer(value: ()).delay(0.1, on: QueueScheduler.main).startWithValues { _ in
+								observer.send(value: input + 1)
+								observer.sendCompleted()
+							}
+						}
+					}
+					var finalValue: Int?
+					action.apply(0)
+						.observe(on: QueueScheduler.main)
+						.flatMap(.latest) {
+							action.apply($0)
+						}.startWithValues {
+							finalValue = $0
+						}
+
+					// Input should be ignored and returns 1
+					action.apply(2).startWithValues {
+						expect($0) == 1
+					}
+
+					expect(finalValue).toEventually(equal(2))
+				}
+			}
 			describe("apply.dispose()") {
 				it("should dispose of all created signal producers") {
 					var startCounter = 0
